5aba9a269b23c7a303ad6ea5a6c6a2c5f7072347
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-12-28 16:55:09 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
     btLaufzeit = input.runningTime()
-    if (bit.between(receivedNumber, 45, 135)) {
+    if (bit.between(receivedNumber, 30, 150)) {
         bit.comment("Servo")
         ServoSteuerung(receivedNumber)
     }
+})
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
 })
 function ServoSteuerung (pWinkel: number) {
     if (!(btConnected)) {
@@ -12,18 +18,21 @@ function ServoSteuerung (pWinkel: number) {
     } else if (iWinkel != pWinkel) {
         bit.comment("connected und Wert geändert")
         iWinkel = pWinkel
-        pins.servoWritePin(AnalogPin.C17, iWinkel)
+        pins.servoWritePin(AnalogPin.C17, iWinkel + 6)
     }
     return iWinkel
 }
 let iWinkel = 0
 let btLaufzeit = 0
 let btConnected = false
+radio.setGroup(240)
 btConnected = true
 btLaufzeit = input.runningTime()
+pins.digitalWritePin(DigitalPin.P0, 1)
+ServoSteuerung(90)
 loops.everyInterval(500, function () {
     bit.comment("Überwachung Bluetooth")
-    if (input.runningTime() - btLaufzeit < 0) {
+    if (input.runningTime() - btLaufzeit < 1000) {
         if (!(btConnected)) {
             btConnected = true
             basic.setLedColor(0x00ff00)
